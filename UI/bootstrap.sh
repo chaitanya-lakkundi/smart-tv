6c69587a9e15930ab2ffad4d1b32100d8b328c62
Chaitanya <chaitanya.lakkundi@gmail.com>
2018-05-27 23:37:48 +0530

    Final Rels

--- UI/bootstrap.sh
+++ UI/bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export FLASK_APP=/home/pi/UI/keystroke_server/index.py
+export PATH="/home/pi/berryconda3/bin:$PATH"
 #source venv/bin/activate
 flask run -h 0.0.0.0 -p 7000
--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#xfce4-terminal -e "/home/pi/UI/bootstrap.sh" &
+xfce4-terminal -e "/home/pi/UI/bootstrap.sh" &
 
 cd ~/UI/website/build
 xfce4-terminal -e "python2 -m SimpleHTTPServer 8000" &
@@ -14,4 +14,3 @@ cd ~/Extract_Logo
 sleep 2
 xfce4-terminal -e "/opt/td-agent-bit/bin/td-agent-bit -c /etc/td-agent-bit/td-agent-bit.conf -R /etc/td-agent-bit/parsers.conf" &
 #./master.py start ~/my_video-1.mkv 10
-xfce4-terminal -x "/home/pi/UI/bootstrap.sh"
